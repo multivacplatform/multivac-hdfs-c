88289de51575b6919f7fcfba1b2d2281e53caa8c
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-11-20 18:50:05 +0100

    Update run_c.sh

--- cloudera/run_c.sh
+++ cloudera/run_c.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 filePath=$1
 export CLASSPATH=`hadoop classpath --glob`
+export LD_LIBRARY_PATH=/usr/lib/jvm/java-8-oracle/jre/lib/amd64/server:/opt/cloudera/parcels/CDH/lib/
+
 echo "Running shell script $filePath"
 ./cpp_exe $filePath
