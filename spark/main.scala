07907ef375e4c7c1ef9d6a6be1f2b73b6f8f8919
Maziyar Panahi <maziyar.panahi@iscpif.fr>
2018-11-20 18:56:53 +0100

    Update main.scala

--- spark/main.scala
+++ spark/main.scala
@@ -1,6 +1,14 @@
 import sys.process._
 import java.nio.file.{ Paths, Files }
 
-var inputs_points  = sc.parallelize(List("/tmp/file1.txt","/tmp/file2.txt","/tmp/file3.txt"));
-val exe_path = Paths.get(".").toAbsolutePath.getParent + "/cpp_exe";
-inputs_points.pipe(exe_path).collect()
+val distScript = "hdfs:///YOUR_HOME/tmp/cpp_exe"
+val bashScript = "hdfs:///YOUR_HOME/tmp/run_c.sh"
+sc.addFile(distScript)
+sc.addFile(bashScript)
+
+val filePath = List("/user/YOUR_HOME/tmp/file.txt","/user/YOUR_HOME/tmp/file2.txt","/user/YOUR_HOME/tmp/file3.txt")
+val partitionCount = filePath.length.toInt
+val inputs_points  = sc.parallelize(filePath).repartition(partitionCount)
+
+val exe_path2 = inputs_points.pipe(Seq("./run_c.sh"))
+exe_path2.collect().map(println(_))
